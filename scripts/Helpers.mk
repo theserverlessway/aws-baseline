456b2f67d0391b4de9d0b1a275bbe54c4b52441c
Florian Motlik <flo@theserverlessway.com>
2019-12-13 19:45:45 +0100

    Update configuration to fix deployment to new accounts

--- scripts/Helpers.mk
+++ scripts/Helpers.mk
@@ -11,4 +11,4 @@ excluded:
 	@ls -d */ | grep -Ff Excluded || true
 
 
-FOR_ALL_DIRS=$(LIST_DIRS) | xargs -n 1 -I {} bash -c "cd {} && echo '--------- {}' && $1 && echo ''"
+FOR_ALL_DIRS=$(LIST_DIRS) | xargs -n 1 -I {} bash -c "(cd {} && echo '--------- {}' && $1 && echo '') || exit 255"
